b6613add8134d664e40b51386af839af00676ffa
robertobotani <rbotanin@gmail.com>
2020-01-15 10:58:44 -0300

    primeiro commit

---
+++ dados.php
@@ -0,0 +1,55 @@
+<?php
+
+
+$email = $_GET["email"] ;
+$password = $_GET["password"];
+$name = $_GET["name"];
+$last_name = $_GET["last_name"];
+$full_name = '$name $last_name';    // string literal
+$full_name = "$name $last_name";   // string avaliada
+
+$int_value = 10 ;// int
+$float_value = 99.99; // float 
+var_dunp ($full_name);
+var_dunp ($int_value);
+var_dunp ($float_value);      
+
+?>
+
+<html>
+    <head>
+        <title>Dados</title>
+        <meta charset="utf-8"/>
+        
+        </head>
+        <body>
+            <p>Usuario Logado: <?php echo $email; ?> - </p>
+            
+            <h1>Recebendo dados</h1>
+            <p>
+                E-mail:<?php echo $email;?>
+            </p>
+            <p>
+                Senha:<?php echo $password;?>
+            </p>
+            <p>  
+              Nome:<?php echo $name;?>
+            </p>
+            <p> 
+            Sobrenome: <?php echo $last_name;?> 
+            </p>
+            
+             <p>
+                E-mail:<?php echo $email; ?>   
+            </p> 
+            <p>
+               Nome Completo:<?php echo $full_name;?>
+            </p>
+           
+            
+        </body>
+         
+    
+</html>
+
+    
